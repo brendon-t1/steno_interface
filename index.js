827398a1246e69b38814d96ba14041b995d3c884
Brendon <soupe.aux.tortues@gmail.com>
2022-10-18 19:51:24 -0700

    steno board

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path')
 
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
-  res.send('type type!')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(port, () => {
